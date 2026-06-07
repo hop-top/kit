ae936baf41e94b6777efa00613b0dacfcbf01650
Jad Bitar <33527+jadb@users.noreply.github.com>
2026-06-06 21:57:21 -0400

    Merge pull request #149 from hop-top/release-please--branches--main--components--kit
    chore(release): kit 0.5.0-alpha.0

---
+++ sdk/experimental/rs/src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(feature = "cli")]
+pub mod cli;
+#[cfg(feature = "output")]
+pub mod output;
+pub mod tui;
+
+#[cfg(feature = "uri")]
+pub mod uri;
+
+#[cfg(feature = "api")]
+pub mod api;
+
+#[cfg(feature = "id")]
+pub mod id;
+
+#[cfg(feature = "telemetry")]
+pub mod telemetry;
